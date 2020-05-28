cace0ebbb413a43b5723ce1591a4f1774ef3b0da
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-05-28 12:00:17 +0200

    Fix package capitalisation

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "irckit",
+    name: "IRCKit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "irckit",
-            targets: ["irckit"]),
+            name: "IRCKit",
+            targets: ["IRCKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,14 +22,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "irckit",
+            name: "IRCKit",
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOSSL", package: "swift-nio-ssl"),
                 .product(name: "NIOExtras", package: "swift-nio-extras")
             ]),
         .testTarget(
-            name: "irckitTests",
-            dependencies: ["irckit"]),
+            name: "IRCKitTests",
+            dependencies: ["IRCKit"]),
     ]
 )
