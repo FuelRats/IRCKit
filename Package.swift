7653653e515d4a12091f8b36903f4cab6f655824
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-09-06 19:34:40 +0200

    Even more fun with case sensitivity

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "IRCKit",
-            targets: ["IRCKit"]),
+            targets: ["irckit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,8 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "IRCKit",
-            path: "Sources/irckit",
+            name: "irckit",
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOSSL", package: "swift-nio-ssl"),
@@ -33,6 +32,6 @@ let package = Package(
             ]),
         .testTarget(
             name: "IRCKitTests",
-            dependencies: ["IRCKit"]),
+            dependencies: ["irckit"]),
     ]
 )
