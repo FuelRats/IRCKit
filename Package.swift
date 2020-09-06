f41c5953fcec027987180974a0206fc44fe07188
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-09-06 19:26:51 +0200

    Prevent the swift compiler from having a seizure on case sensitive file systems

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IRCKit",
+            path: "Sources/IRCKit",
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOSSL", package: "swift-nio-ssl"),
