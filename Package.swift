fa21c5fa8b779bd3d2741e25165b656cdb28acf4
Alex S. Glomsaas <alex@sorlie.io>
2021-05-21 04:13:26 +0200

    Update dependencies for Swift 5.4 compatibility

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "IRCKit",
-    
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -18,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: Version(2, 22, 0)),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: Version (2, 9, 1)),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: Version(1, 7, 0)),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: Version(1, 3, 1)),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: Version(1, 4, 0)),
         .package(url: "https://github.com/gonzalezreal/DefaultCodable.git", from: Version(1, 2, 0))
     ],
     targets: [
